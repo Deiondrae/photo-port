b8e9c4f131f746a247ed39e01543e536d8860b40
Villager <deiondrae@gmail.com>
2023-02-10 15:35:02 -0500

    UI updates based on user selection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import "./App.css";
+import { useState } from "react";
 import About from "./components/about";
 import Nav from "./components/nav";
 import Gallery from "./components/gallery";
 
 function App() {
+  const [categories] =  useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+  const [currentCategory, setcurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <Nav></Nav>
+      <Nav 
+      categories={categories}
+      setcurrentCategory={setcurrentCategory}
+      currentCategory={currentCategory}></Nav>
       <main>
         <Gallery></Gallery>
         <About></About>
