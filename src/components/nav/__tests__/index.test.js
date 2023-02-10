bfa91ead1835e9ed04e19d295e0fdf99503fc328
Villager <deiondrae@gmail.com>
2023-02-10 12:54:23 -0500

    nav testing

--- src/components/nav/__tests__/index.test.js
+++ src/components/nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
