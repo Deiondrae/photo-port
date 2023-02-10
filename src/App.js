f01bca983a1df4fac15a6f87c14f68b23ca1c587
Villager <deiondrae@gmail.com>
2023-02-10 14:14:47 -0500

    added gallery component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/about";
-import Nav from "./components/nav"
+import Nav from "./components/nav";
+import Gallery from "./components/gallery";
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
